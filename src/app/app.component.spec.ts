25d4ae6e9f25f7b2b57b082e6160124943184c7e
NIkhil <150216373+Nikhil1823@users.noreply.github.com>
2025-09-05 23:31:18 +0530

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,5 @@
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Forms-project');
-  });
-});
+ 
 
 
 
