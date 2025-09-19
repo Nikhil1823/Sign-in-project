17e282a2d4c5ee2b8aa0c197b02cc3d2436cce4d
NIkhil <150216373+Nikhil1823@users.noreply.github.com>
2025-09-19 21:22:15 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import { AppComponent } from './app/app.component';
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
-//add obe more comment
+
 //add one more comment
 //add one more also
+
 
 
